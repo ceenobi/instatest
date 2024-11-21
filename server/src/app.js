2ef052da090f010273beca0581c16302429848ed
ceenobi <cobimbachu@gmail.com>
2024-11-22 00:53:07 +0100

    fixed cors

--- server/src/app.js
+++ server/src/app.js
@@ -9,7 +9,7 @@ import authRoutes from "./routes/user.js";
 const app = express();
 
 const corsOptions = {
-  origin: ["http://localhost:5173", "http://localhost:5174"],
+  origin: ["http://localhost:5173", "http://localhost:5174", "https://instapics.vercel.app"],
   optionsSuccessStatus: 200,
   methods: ["GET", "POST", "PATCH", "DELETE"],
   credentials: true,
